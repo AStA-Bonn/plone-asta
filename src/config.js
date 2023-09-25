d20dc1eedb583ff01b18816c9f93fe9eeae7937a
Felix Ulonska <git@webfoo.de>
2023-09-25 17:39:43 +0200

    init

---
+++ src/config.js
@@ -0,0 +1,24 @@
+/**
+ * Add your config changes here.
+ * @module config
+ * @example
+ * export default function applyConfig(config) {
+ *   config.settings = {
+ *     ...config.settings,
+ *     port: 4300,
+ *     listBlockTypes: {
+ *       ...config.settings.listBlockTypes,
+ *       'my-list-item',
+ *    }
+ * }
+ */
+
+// All your imports required for the config here BEFORE this line
+import '@plone/volto/config';
+
+export default function applyConfig(config) {
+  config.settings.isMultilingual = true;
+  config.settings.supportedLanguages = ['de', 'en'];
+  config.settings.defaultLanguage = 'de';
+  return config;
+}
