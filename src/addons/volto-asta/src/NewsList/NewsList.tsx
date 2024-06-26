b8421e634906b0647103fee5ba184a2756c86386
Felix Ulonska <git@webfoo.de>
2024-06-26 14:07:03 +0200

    feat: news Item

--- src/addons/volto-asta/src/NewsList/NewsList.tsx
+++ src/addons/volto-asta/src/NewsList/NewsList.tsx
@@ -1,18 +1,65 @@
 import React from "react";
 import "./NewsList.scss";
-import { UniversalLink } from "@plone/volto/components";
-import { FormattedMessage } from "react-intl";
+import { getMessages } from "../Referat/lang";
+import { useIntl } from "react-intl";
+import { When } from "@plone/volto/components/theme/View/EventDatesInfo";
+import { Icon } from "@plone/volto/components";
+import mapSvg from "@plone/volto/icons/map.svg";
+import chronoSvg from "@plone/volto/icons/chrono.svg";
+
+export const EventSubtitle = ({ item }) => {
+  return (
+    <>
+      <div className="icon-line">
+        <Icon name={chronoSvg} size="1.5rem" />
+        <When
+          start={item.start}
+          end={item.end}
+          whole_day={item.whole_day}
+          open_end={true}
+        />
+      </div>
+      <div className="icon-line">
+        <Icon name={mapSvg} size="1.5rem" />{" "}
+        <div>{item.location ?? "Wird noch bekannt gegeben"}</div>
+      </div>
+      <div style={{ padding: "0.25rem" }} />
+    </>
+  );
+};
+
+export const NewsItem = ({ item }) => {
+  const intl = useIntl();
+  const messages = getMessages(intl.locale);
+  const creationDate = new Date(item.CreationDate);
+  const isNews = !item.start;
+  return (
+    <a key={item["@id"]} href={item["@id"]} className="card unstyled-link">
+      <div className="container">
+        <h3 className="title">{item.title}</h3>
+        {isNews && (
+          <p className="subtitle">
+            {creationDate.getDate()}.{creationDate.getMonth() + 1}.
+            {creationDate.getFullYear()}
+          </p>
+        )}
+        {!!item.start && <EventSubtitle item={item} />}
+        {!!item.description && (
+          <>
+            <span>{item.description}</span> <br />
+          </>
+        )}
+        <a key={item["@id"]}>{messages.more}</a>
+      </div>
+    </a>
+  );
+};
 
 export const NewsList = ({ items, ...props }) => {
   return (
     <>
       {items.map((item) => (
-        <a key={item["@id"]} href={item["@id"]} className="card">
-          <div className="container">
-            <h3>{item.title}</h3>
-            <span>{item.description}</span>
-          </div>
-        </a>
+        <NewsItem key={item["@id"]} item={item} />
       ))}
     </>
   );
--- src/addons/volto-asta/src/Referat/lang.ts
+++ src/addons/volto-asta/src/Referat/lang.ts
@@ -1,69 +1,74 @@
 /* Plone i18n is insuffarable. There self impl. Way easier to mantain.
-*/
+ */
 export interface MESSAGES {
-        days: {
-                monday: string;
-                tuesday: string;
-                wednesday: string;
-                thursday: string;
-                friday: string;
-                saturday: string;
-                sunday: string;
-        }
-        attr: {
-                phone: string;
-                email: string;
-                place: string;
-                staff: string;
-                attendance_times: string;
-        }
+  days: {
+    monday: string;
+    tuesday: string;
+    wednesday: string;
+    thursday: string;
+    friday: string;
+    saturday: string;
+    sunday: string;
+  };
+  attr: {
+    phone: string;
+    email: string;
+    place: string;
+    staff: string;
+    attendance_times: string;
+  };
+  more: string;
+  yetToBeAnnounced: string;
 }
 
 export function getMessages(locale: string): MESSAGES {
-        switch(locale) {
-                case 'de':
-                        return MESSAGES_DE;
-                case 'en':
-                default:
-                        return MESSAGES_EN;
-        }
+  switch (locale) {
+    case "de":
+      return MESSAGES_DE;
+    case "en":
+    default:
+      return MESSAGES_EN;
+  }
 }
 
 export const MESSAGES_EN: MESSAGES = {
-    days: {
-        monday: "Monday",
-        tuesday: "Tuesday",
-        wednesday: "Wednesday",
-        thursday: "Thursday",
-        friday: "Friday",
-        saturday: "Saturday",
-        sunday: "Sunday",
-    },
-    attr: {
-        phone: "Phone",
-        email: "Email",
-        place: "Place",
-        staff: "Staff",
-        attendance_times: "Attendance Times",
-    }
-}
+  days: {
+    monday: "Monday",
+    tuesday: "Tuesday",
+    wednesday: "Wednesday",
+    thursday: "Thursday",
+    friday: "Friday",
+    saturday: "Saturday",
+    sunday: "Sunday",
+  },
+  attr: {
+    phone: "Phone",
+    email: "Email",
+    place: "Place",
+    staff: "Staff",
+    attendance_times: "Attendance Times",
+  },
+  more: "more",
+  yetToBeAnnounced: "Yet to be announced",
+};
 
 export const MESSAGES_DE: MESSAGES = {
-    days: {
-        monday: "Montag",
-        tuesday: "Dienstag",
-        wednesday: "Mittwoch",
-        thursday: "Donnerstag",
-        friday: "Freitag",
-        saturday: "Samstag",
-        sunday: "Sonntag",
-    },
-    attr: {
-        phone: "Telefon",
-        email: "E-Mail",
-        place: "Ort",
-        staff: "Mitarbeitende",
-        attendance_times: "Anwesenheitszeiten",
-    }
-}
-
+  days: {
+    monday: "Montag",
+    tuesday: "Dienstag",
+    wednesday: "Mittwoch",
+    thursday: "Donnerstag",
+    friday: "Freitag",
+    saturday: "Samstag",
+    sunday: "Sonntag",
+  },
+  attr: {
+    phone: "Telefon",
+    email: "E-Mail",
+    place: "Ort",
+    staff: "Mitarbeitende",
+    attendance_times: "Anwesenheitszeiten",
+  },
+  more: "mehr",
+  yetToBeAnnounced: "Wird noch bekannt gegeben",
+};
