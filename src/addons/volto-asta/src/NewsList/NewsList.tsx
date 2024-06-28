9ce0f5494b09d9c76bbd591c2702180bfc140189
Felix Ulonska <git@webfoo.de>
2024-06-28 11:19:45 +0200

    fix(newsitem): fixed padding in event card

--- src/addons/volto-asta/src/NewsList/NewsList.tsx
+++ src/addons/volto-asta/src/NewsList/NewsList.tsx
@@ -6,22 +6,32 @@ import { When } from "@plone/volto/components/theme/View/EventDatesInfo";
 import { Icon } from "@plone/volto/components";
 import mapSvg from "@plone/volto/icons/map.svg";
 import chronoSvg from "@plone/volto/icons/chrono.svg";
+import { format } from "prettier";
+
+function toHumanReadableDate(date: Date, includeTime = false) {
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+
+  let formattedDate = `${day}.${month}.${year}`;
+
+  if (includeTime) {
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    formattedDate += ` ${hours}:${minutes}`;
+  }
+  return formattedDate;
+}
 
 export const EventSubtitle = ({ item }) => {
   return (
     <>
       <div className="icon-line">
         <Icon name={chronoSvg} size="1.5rem" />
-        <When
-          start={item.start}
-          end={item.end}
-          whole_day={item.whole_day}
-          open_end={true}
-        />
+        {toHumanReadableDate(new Date(item.start), true)}
       </div>
       <div className="icon-line">
-        <Icon name={mapSvg} size="1.5rem" />{" "}
-        <div>{item.location ?? "Wird noch bekannt gegeben"}</div>
+        <Icon name={mapSvg} size="1.5rem" /> <div>{item.location ?? "Wird noch bekannt gegeben"}</div>
       </div>
       <div style={{ padding: "0.25rem" }} />
     </>
@@ -37,12 +47,7 @@ export const NewsItem = ({ item }) => {
     <a key={item["@id"]} href={item["@id"]} className="card unstyled-link">
       <div className="container">
         <h3 className="title">{item.title}</h3>
-        {isNews && (
-          <p className="subtitle">
-            {creationDate.getDate()}.{creationDate.getMonth() + 1}.
-            {creationDate.getFullYear()}
-          </p>
-        )}
+        {isNews && <p className="subtitle">{toHumanReadableDate(creationDate)}</p>}
         {!!item.start && <EventSubtitle item={item} />}
         {!!item.description && (
           <>
